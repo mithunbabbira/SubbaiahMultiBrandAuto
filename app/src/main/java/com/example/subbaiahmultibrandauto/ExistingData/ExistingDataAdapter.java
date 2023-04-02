4da118fef751db877ca4c170f05eff0982405cde
mithun_babbira <mithunbabbira@gmail.com>
2023-04-02 11:50:19 +0530

    ADDED laoding page

--- app/src/main/java/com/example/subbaiahmultibrandauto/ExistingData/ExistingDataAdapter.java
+++ app/src/main/java/com/example/subbaiahmultibrandauto/ExistingData/ExistingDataAdapter.java
@@ -1,48 +1,31 @@
 package com.example.subbaiahmultibrandauto.ExistingData;
-
-import android.app.ExpandableListActivity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.subbaiahmultibrandauto.R;
 import com.example.subbaiahmultibrandauto.entities.Data;
-
 import java.util.List;
-
 public class ExistingDataAdapter  extends RecyclerView.Adapter<ExistingDataAdapter.ViewHolder>{
-
     private Context context ;
     private  List<Data> dataList ;
-
-
     public ExistingDataAdapter (Context context, List<Data> dataList) {
         this.context = context  ;
         this.dataList = dataList;
-
-
     }
-
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from(context).inflate(R.layout.adapter_existing_data, parent, false);
         return new ViewHolder(view);
 
     }
-
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
         holder.date.setText(dataList.get(position).getDate());
-
     }
 
     @Override
@@ -51,17 +34,10 @@ public class ExistingDataAdapter  extends RecyclerView.Adapter<ExistingDataAdapt
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
         TextView date;
-
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
             date =  itemView.findViewById(R.id.date);
-
-
-
 //            itemView.setOnClickListener(this);
 //            itemView.setOnCreateContextMenuListener(this);
         }
--- app/src/main/java/com/example/subbaiahmultibrandauto/MainActivity.java
+++ app/src/main/java/com/example/subbaiahmultibrandauto/MainActivity.java
@@ -28,66 +28,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         mStore = FirebaseFirestore.getInstance();
         activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
-
-
-
-
-
-//        mStore.collection("VehicleNo").document("KA12M1703").set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
-//            @Override
-//            public void onSuccess(Void unused) {
-//                Toast.makeText(MainActivity.this ,"success", Toast.LENGTH_SHORT).show();
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Toast.makeText(MainActivity.this ,"EXCEPTION", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
 
         activityMainBinding.submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                activityMainBinding.rlLoader.setVisibility(View.VISIBLE);
                 checkVehicleExistence(activityMainBinding.getVehicleNo.getText());
             }
         });
     }
     private void checkVehicleExistence(Editable text) {
-
         CollectionReference collectionReference = mStore.collection("VehicleNo").document(String.valueOf(text)).collection("data");
-//        docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//
-//                if (task.isSuccessful()) {
-//                            QuerySnapshot document = task.getResult();
-//                                            document.toObjects()
-//
-//                                Data d = document.toObject(Data.class);
-//                                Toast.makeText(MainActivity.this, "DocumentSnapshot data: " + document.getData(), Toast.LENGTH_SHORT).show();
-//                                Log.d("te", "DocumentSnapshot data: " + document.getData());
-//
-//                                Toast.makeText(MainActivity.this, "DocumentSnapshot data: " + d.getUpcomingRepair(), Toast.LENGTH_SHORT).show();
-//                            }}
-//
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//
-//            }
-//        });
-
         collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-
+                    activityMainBinding.rlLoader.setVisibility(View.GONE);
                     List<Data> dataList = task.getResult().toObjects(Data.class);
                     Intent myIntent;
                     if(dataList.size()!=0){
@@ -99,45 +57,10 @@ public class MainActivity extends AppCompatActivity {
                         myIntent.putExtra("newVehicleNumber",String.valueOf(text));
                     }
                     startActivity(myIntent);
-
-
-
                 }
             }
         }).addOnFailureListener(e -> {
-
+            activityMainBinding.rlLoader.setVisibility(View.GONE);
         });
-
-
-//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//
-//                if(task.isSuccessful()){
-//                    DocumentSnapshot document = task.getResult();
-//                    if(document.exists()){
-//
-//
-//                    }else{
-////                        Data d = (Data) document.toObjects(Data.class);
-//                        Data d = document.toObject(Data.class);
-//                        Intent myIntent = new Intent(MainActivity.this, AddNewVehicle.class);
-//                        myIntent.putExtra("newVehicleNumber",String.valueOf(text));
-//                        startActivity(myIntent);
-//
-//                    }
-//
-//                }
-//
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//
-//            }
-//        });
-
-
-
     }
 }
